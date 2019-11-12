f0f3b5beda293d0b4fdd799397879800162881aa
Supanat Wongchai <o_memaster@hotmail.com>
2019-11-12 14:36:47 +0700

    add dict to project

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const wordBreak = require("word-break-trie");
 const trie = require("trie-prefix-tree");
+const fileSync = require("fs");
 
 const THAI_REGEX = /([\u0E00-\u0E7F]{2,})/;
 
@@ -14,12 +15,18 @@ const correctContent = content => {
   return content.replace(/ํา/g, "ำ");
 };
 
-function thaiTypoCheck(inputText, dict) {
+function thaiTypoCheck(inputText) {
   // return true when input is empty string, null, or undefined
   if (!inputText) return true;
 
-  const longestWordLength = dict[0].length;
-  const currentTrie = trie(dict);
+  const dict = fileSync.readFileSync("./dictionary/dict.txt", {
+    encoding: "UTF-8"
+  });
+
+  const dictArray = dict.split("\n");
+
+  const longestWordLength = dictArray[0].length;
+  const currentTrie = trie(dictArray);
   const inputs = sentenceSplit(correctContent(inputText));
 
   for (let input of inputs) {
